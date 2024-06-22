a178754b56b7d8699dd3b1f2dff2f08096ed889b
fzahiragic1 <farukzahiragic@ymail.com>
2024-06-22 21:20:59 +0200

    Code refactoring and cleanup.

--- Najnovije/glavni.py
+++ Najnovije/glavni.py
@@ -1,9 +1,9 @@
-from machine import SPI, Pin, I2C
+from machine import SPI, Pin, I2C, Timer
 from mqtt_stuff import MQTTCONN
 from image import Image_holder
 import display_driver_utils
 from rotary import Rotary
-from orientation import *
+from orientation import Orientation
 from bmm150 import BMM150
 from bma2x2 import BMA2X2
 import usys as sys
@@ -13,26 +13,24 @@ import math
 import time
 from data import *
 
+# Inicijalizacija MQTT konekcija
 mqtt = MQTTCONN(SSID, PASSWORD)
 
+# Inicijalizacija I2C i SPI objekata
 i2c = I2C(id=I2C_ID, sda=Pin(I2C_SDA), scl=Pin(I2C_SCL), freq=I2C_FREQ)
 print(i2c.scan())
-
 spi = SPI(SPI_ID, baudrate=SPI_BAUDRATE, sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
 drv = ili9xxx.Ili9341(spi=spi, dc=DISPLAY_DC, cs=DISPLAY_CS, rst=DISPLAY_RST)
 
+# Inicijalizacija varijabli
 last_interrupt_time = 0
-
 target = 0
 mode = 0
 target_radians = (0, 0)
-
 update_screen_timer = Timer()
-
 lv.init()
 
-######################## ORIENTING SCREEN ################################
-
+# Konfiguracija glavnog prikaza na displeju
 orienting_screen = lv.obj()
 orienting_screen.set_style_bg_color(lv.color_hex(BACKGROUND_COLOR_BLUE), 0)
 
@@ -53,25 +51,21 @@ large_circle.set_style_border_width(2, 0)
 
 image = Image_holder(orienting_screen)
 
-######################## MENU SCREEN ################################
-
+# Konfiguracija meni prikaza
 menu_screen = lv.obj()
 unos_label = lv.label(menu_screen)
 unos_label.set_text(MENU_SCREEN_TEXT)
 unos_label.align(lv.ALIGN.CENTER, 0, 0)
 
-######################################################################
-
+# Cekanje na inicijalizaciju komponenti
 time.sleep(2)
 
+# Inicijalizacija senzora
 acc = BMA2X2(i2c)
 mag = BMM150(i2c)
-
 orient = Orientation(acc, mag)
 
 def update_screen(t):
-    global target_radians
-    
     current_azimuth = orient.get_azimuth()
     current_latitude = orient.get_latitude()
     
@@ -86,60 +80,54 @@ def update_screen(t):
     
     current_coords_label.set_text(f"Az, Lat: {azimuth}, {latitude}")
     lv.task_handler()
-    
-lv.screen_load(menu_screen)
-    
+
 def encoder_click_handler(t):
-    global last_interrupt_time, target, TARGETS, mode, mqtt, target_radians, image, update_screen_timer, orient
-    global menu_screen, orienting_screen, r_encoder, current_coords_label
-    
+    global last_interrupt_time, target, mode, target_radians
     
     current_time = time.ticks_ms()
-        
     if time.ticks_diff(current_time, last_interrupt_time) > DEBOUNCE_TIME_MS:
         last_interrupt_time = current_time
         if mode == 0:
-            # Set target
+            # Postavljanje mete
             target = r_encoder.get_target()
-
-            # Load the planet image and orientation screen
             image.load_image(TARGETS[target])
-            lv.screen_load(orienting_screen)
+            lv.scr_load(orienting_screen)
             
-            # Fetch initial coordinates from mqtt and set labels
+            # Dobavljanje inicijalnih koordinata iz mqtt-a i postavljanje labela
             mqtt.request(target)
             azimuth, latitude = mqtt.get_coordinates()
             target_radians = (math.radians(azimuth), math.radians(latitude))
             target_coords_label.set_text(f"{TARGETS[target]}: {azimuth}, {latitude}") 
             
-            # Start timers for orientation
+            # Postavljanje tajmera za orijentaciju
             update_screen_timer.init(period=200, callback=update_screen)
             orient.start_reading(period=100)
             
-            # Disable rotation on encoder, current LED stays fixed
+            # Iskljucenje rotacije na enkoderu
             r_encoder.disable_rotation_handler()
             
-            # Change mode
+            # Izmjena moda
             mode = 1
         else:
-            # Remove image from memory
+            # Brisanje slike iz memorije
             image.remove_image()
             
-            # Deinitialize timers
+            # Deinicijalizacija tajmera
             orient.end_reading()
             update_screen_timer.deinit()
             
-            # Enable rotation on encoder
+            # Ukljucenje rotacije na enkoderu
             r_encoder.enable_rotation_handler()
             
-            # Load menu screen and change mode
-            lv.screen_load(menu_screen)
+            # Ukljucenje meni prikaza i postavljanje moda
+            lv.scr_load(menu_screen)
             mode = 0
 
-
+# Inicijalizacija rotacijskog enkodera
 r_encoder = Rotary(CLK_PIN, DT_PIN, SW_PIN, encoder_click_handler)
 r_encoder.enable_rotation_handler()
 r_encoder.enable_click_handler()
 
+# Glavna petlja
 while True:
     pass
